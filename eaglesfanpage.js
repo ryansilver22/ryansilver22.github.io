e828fdf605e073a19df8edf85e2eee29c74ee650
ryansilver22 <112674367+ryansilver22@users.noreply.github.com>
2022-12-01 20:29:59 -0800

    Update eaglesfanpage.js

--- eaglesfanpage.js
+++ eaglesfanpage.js
@@ -73,24 +73,20 @@ window.addEventListener("load", () => {
       const api = "6d055e39ee237af35ca066f35474e9df";
   
       // API URL
-      const base =
-`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&` +
-`lon=${lon}&appid=6d055e39ee237af35ca066f35474e9df`;
+      const base = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&` + `lon=${lon}&appid=6d055e39ee237af35ca066f35474e9df`;
   
       // Calling the API
       fetch(base)
         .then((response) => {
-          return response.json();
+         return response.json();
         })
         .then((data) => {
           console.log(data);
-          temperature.textContent = 
-              Math.floor(data.main.temp - kelvin) + "°C";
+          temperature.textContent = Math.floor(data.main.temp - kelvin) + "°C";
           summary.textContent = data.weather[0].description;
           loc.textContent = data.name + "," + data.sys.country;
           let icon1 = data.weather[0].icon;
-          icon.innerHTML = 
-              `<img src="icons/${icon1}.svg" style= 'height:10rem'/>`;
+          icon.innerHTML = `<img src="icons/${icon1}.svg" style= 'height:10rem'/>`;
         });
     });
   }
